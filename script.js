950afc5d20b1bdf48255131b00ed12a3c7ea1ea1
olivierdg2 <36258928+olivierdg2@users.noreply.github.com>
2019-03-21 22:35:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -91,22 +91,22 @@ function loop() {
 document.addEventListener('keydown', function(e) {
 
     if (e.which === 37 && snake.dx === 0) {
-        snake.dx = -50;
+        snake.dx = -grid;
         snake.dy = 0;
     }
 
     else if (e.which === 38 && snake.dy === 0) {
-        snake.dy = -50;
+        snake.dy = -grid;
         snake.dx = 0;
     }
 
     else if (e.which === 39 && snake.dx === 0) {
-        snake.dx = 50;
+        snake.dx = grid;
         snake.dy = 0;
     }
 
     else if (e.which === 40 && snake.dy === 0) {
-        snake.dy = 50;
+        snake.dy = grid;
         snake.dx = 0;
     }
 });
